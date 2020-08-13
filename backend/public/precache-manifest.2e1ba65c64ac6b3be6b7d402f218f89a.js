659e1368a031e2b4eb62ea40edd291797456f9ff
cebasypia <cebasypia.tskm8@gmail.com>
2020-08-13 21:40:19 +0900

    Update build

---
+++ backend/public/precache-manifest.2e1ba65c64ac6b3be6b7d402f218f89a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f38fb84527b4093a68049841ab28da3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9932dd52937d860b3a0",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "94c363d8165d65b04a32",
+    "url": "/static/js/2.c7955622.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.c7955622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9932dd52937d860b3a0",
+    "url": "/static/js/main.c111df0e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
